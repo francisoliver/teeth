9e3fdda076b216225f40298fd50a6aeee0f640fb
Cedric Sarmenta <cedric.sarmenta@louis.com>
2015-04-18 12:13:39 +0800

    Added scaffolds for UserGroup and UserEvent

---
+++ teeth-api/grails-app/controllers/com/teeth/api/UserEventController.groovy
@@ -0,0 +1,5 @@
+package com.teeth.api
+
+class UserEventController {
+     static scaffold = true
+}
---
+++ teeth-api/grails-app/controllers/com/teeth/api/UserGroupController.groovy
@@ -0,0 +1,5 @@
+package com.teeth.api
+
+class UserGroupController {
+    static scaffold = true
+}
