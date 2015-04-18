4850cd1773521e31189404db2c8e4f7e01f34df0
Chester Troy Baluyo <chester.baluyo@aestrea.com>
2015-04-18 19:40:29 +0800

    WIP

--- teeth-api/grails-app/services/com/teeth/api/ExternalApiService.groovy
+++ teeth-api/grails-app/services/com/teeth/api/ExternalApiService.groovy
@@ -27,14 +27,14 @@ class ExternalApiService {
             requestContentType = ContentType.TEXT
 
             body = """{
-                "email": "chester.troy@email.com",
-                "password": "12345",
-                "firstname": "chester",
-                "lastname": "troy",
-                "gender": "male",
-                "alias": "teroy",
-                "birthdate": "09221987",
-                "mobile":"09091234444"
+                    "email": "chester.troy@email.com",
+                    "password": "12345",
+                    "firstname": "chester",
+                    "lastname": "troy",
+                    "gender": "male",
+                    "alias": "teroy",
+                    "birthdate": "09221987",
+                    "mobile":"09091234444"
             }"""
 
             response?.success = { resp, json ->
@@ -51,40 +51,65 @@ class ExternalApiService {
     }
 
     def login() {
-        RESTClient client = new RESTClient("http://206.80.55.76:8080")
-        def data = client.post(path: '/HackathonWSAPI/hackathon/loginservice/login', requestContentType: ContentType.JSON,
-                body:
-                        """{
-                "email": "chester.troy@email.com",
-                "password": "12345",
-            }"""
-        )
-//        def data
-//        URIBuilder uri = new URIBuilder("http://206.80.55.76:8080")
-////        uri.addQueryParam(getCredentials())
-//
-//        HTTPBuilder httpBuilder = new HTTPBuilder(uri)
-//
-//        httpBuilder.request(Method.POST) { req ->
-//            uri.path = "/HackathonWSAPI/hackathon/loginservice/login"
-//
-//            requestContentType = ContentType.JSON
-//
-//            body = """{
+//        RESTClient client = new RESTClient("http://206.80.55.76:8080")
+//        def data = client.post(path: '/HackathonWSAPI/hackathon/loginservice/login', requestContentType: ContentType.JSON,
+//                body:
+//                        """{
 //                "email": "chester.troy@email.com",
 //                "password": "12345",
 //            }"""
+//        )
+        def data
+        URIBuilder uri = new URIBuilder("http://206.80.55.76:8080")
+//        uri.addQueryParam(getCredentials())
+
+
+        HTTPBuilder httpBuilder = new HTTPBuilder(uri)
+
+        httpBuilder.request(Method.POST) { req ->
+            uri.path = "/HackathonWSAPI/hackathon/loginservice/login"
+
+
+
+            requestContentType = ContentType.TEXT
+
+            body = """{
+                "email": "chester.troy@email.com",
+                "password": "12345",
+            }"""
+
+            response?.success = { resp, reader ->
 //
-//            response?.success = { resp, reader ->
-////
-////              data = json.data
-//                resp
-//                reader.getText()
-//            }
-//
-//
-//        }
+//              data = json.data
+                resp
+                data = getData(resp.responseBase.entity.wrappedEntity.getContent())
+            }
+
+            response.failure = {resp, reader ->
+                resp
+                reader
+            }
+
+
+        }
 
         data
+    }
+
+    private static String getData(def content) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(content));
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        }
+        catch (IOException e) { e.printStackTrace(); }
+        catch (Exception e) { e.printStackTrace(); }
+
+        sb.toString()
     }
 }
