57ebc2b11d87e1e80eea8acb6c707abc41081d26
francis.malit <francis.malit@aestrea.com>
2015-04-18 21:28:04 +0800

    Create pojo for login

---
+++ app/src/main/java/com/teeth/app/model/Login.java
@@ -0,0 +1,15 @@
+package com.teeth.app.model;
+
+
+public class Login {
+
+    String username;
+    String password;
+
+
+    public Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+}
