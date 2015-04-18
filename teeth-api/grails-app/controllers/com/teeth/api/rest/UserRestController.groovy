2c0e60f73141443b832cdd5782a9cd20cdfce8e3
Chester Troy Baluyo <chester.baluyo@aestrea.com>
2015-04-18 15:47:23 +0800

    WIP, implement api service

--- teeth-api/grails-app/controllers/com/teeth/api/rest/UserRestController.groovy
+++ teeth-api/grails-app/controllers/com/teeth/api/rest/UserRestController.groovy
@@ -1,19 +1,24 @@
 package com.teeth.api.rest
 
 import com.teeth.api.User
-
+import com.teeth.api.ExternalApiService
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
 
 @Transactional(readOnly = true)
 class UserRestController {
 
+    ExternalApiService externalApiService
     static responseFormats = ['json', 'xml']
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
-        respond User.list(params), [status: OK]
+
+        def response = externalApiService.register()
+
+        render response
+//        respond User.list(params), [status: OK]
     }
 
     def show() {
