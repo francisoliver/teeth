81bdfde6476c0a419f3ae266b3453002138f276c
Chester Troy Baluyo <chester.baluyo@aestrea.com>
2015-04-18 16:34:23 +0800

    Using httpBuilder

--- teeth-api/grails-app/services/com/teeth/api/ExternalApiService.groovy
+++ teeth-api/grails-app/services/com/teeth/api/ExternalApiService.groovy
@@ -1,45 +1,50 @@
 package com.teeth.api
 
+import com.opensymphony.sitemesh.Content
 import grails.converters.JSON
 import grails.transaction.Transactional
+import groovyx.net.http.ContentType
 import groovyx.net.http.HTTPBuilder
 import groovyx.net.http.Method
 import groovyx.net.http.RESTClient
 import groovyx.net.http.URIBuilder
-import org.apache.http.entity.ContentType
 
 @Transactional
 class ExternalApiService {
 
     def register() {
-        RESTClient client = new RESTClient("http://206.80.55.76:8080")
-        def response = client.post(path: '/HackathonWSAPI/hackathon/loginservice/register', requestContentType: ContentType.APPLICATION_JSON,
-              body: [
-                email: "chester.troy@email.com",
-                password: "12345",
-                firstname: "chester",
-                lastname: "troy",
-                gender: "male",
-                alias: "teroy",
-                birthdate: "09221987",
-                mobile:"09091234444"
-            ]
-        )
+        def data
+        URIBuilder uri = new URIBuilder("http://206.80.55.76:8080")
+//        uri.addQueryParam(getCredentials())
 
-//        URIBuilder uri = new URIBuilder("https://api.github.com")
-////        uri.addQueryParam(getCredentials())
-//
-//        HTTPBuilder httpBuilder = new HTTPBuilder(uri)
-//
-//        def response
-//        httpBuilder.request(Method.GET, groovyx.net.http.ContentType.JSON) { req ->
-//            uri.path = "https://api.github.com/user/cedrickimsarmenta"
-//
-//            response?.success = { resp, json ->
-//                data = json.data
-//            }
-//        }
+        HTTPBuilder httpBuilder = new HTTPBuilder(uri)
 
-        response
+        httpBuilder.request(Method.POST) { req ->
+            uri.path = "/HackathonWSAPI/hackathon/loginservice/register"
+
+            requestContentType = ContentType.TEXT
+
+            body = """{
+                "email": "chester.troy@email.com",
+                "password": "12345",
+                "firstname": "chester",
+                "lastname": "troy",
+                "gender": "male",
+                "alias": "teroy",
+                "birthdate": "09221987",
+                "mobile":"09091234444"
+            }"""
+
+            response?.success = { resp, json ->
+//
+//              data = json.data
+                resp
+                json
+            }
+
+
+        }
+
+        data
     }
 }
